f3d292f6ece776ca20aa73aa888d25d4058b366d
Nam Eungyeong <nek1717@naver.com>
2024-02-20 17:05:15 +0900

    feat: nodejs 샌드박스 환경 구성

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,19 +1,30 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import ivm from "isolated-vm";
 
 const app: Application = express();
 
 const port: number = 3001;
 
 app.use(cors());
+// app.use(express.text({ type: () => true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 //TODO: 같은 디렉터리에서 가상환경을 구축
-
 app.post("/todo", (req: Request, res: Response) => {
-  // res.send("Hello toto");
-  console.log(req.body);
+  try {
+    const { code: testCode, lang } = req.body;
+    const code = `(function() { return ${testCode}; })()`;
+    const isolate = new ivm.Isolate({ memoryLimit: 8 });
+    const script = isolate.compileScriptSync(code);
+    const context = isolate.createContextSync();
+    const result = script.runSync(context);
+    // console.log(req.body);
+    res.status(200).send(`${result}`);
+  } catch (e) {
+    console.error(e);
+  }
 });
 
 app.listen(port, function () {
