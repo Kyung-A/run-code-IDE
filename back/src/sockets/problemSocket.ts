0a25d5a9bc424b548e93b06ea45fe6f6d69d6225
Nam Eungyeong <nek1717@naver.com>
2024-04-04 17:48:57 +0900

    feat: js코드를 docker 환경에서 실행되게 로직 수정

--- back/src/sockets/problemSocket.ts
+++ back/src/sockets/problemSocket.ts
@@ -1,7 +1,6 @@
-import { exec } from "child_process";
+import { exec, execSync } from "child_process";
 import fs from "fs";
 
-import javascriptRun from "../compile/javascript";
 import { problemTestcase } from "../mock/problem";
 
 export const problemSocket = (io: any) => {
@@ -14,53 +13,76 @@ export const problemSocket = (io: any) => {
     socket.on("join", async (data: any) => {
       socket.room = data.room;
       const { id, code, lang } = data;
-
-      let result: string[] = [];
       const testcase = problemTestcase.find((v) => v.id === id);
+      let clientResult: { [key: string]: boolean | null } = {};
 
       switch (lang) {
         case "javascript":
-          result = await javascriptRun(socket, code, testcase);
+          fs.writeFileSync("code.js", code);
+
+          testcase?.testcase.forEach((test, i) => {
+            clientResult[i] = null;
+            const input = test.input;
+            fs.writeFileSync("input.txt", input as string);
+            const command = `docker run --rm -v %cd%:/dist node:latest node /dist/code.js /dist/input.txt`;
+
+            try {
+              const result = execSync(command);
+              const value = JSON.parse(result.toString());
+
+              if (value === test.output) {
+                clientResult[i] = true;
+                socket.emit("test", clientResult);
+              } else {
+                clientResult[i] = false;
+                socket.emit("test", clientResult);
+              }
+            } catch (error) {
+              console.error(`Error: ${(error as any).message}`);
+              socket.emit(
+                "error",
+                (error as any).message.split("SyntaxError:")[1].split("at")[0]
+              );
+            }
+          });
           break;
         case "python":
           fs.writeFileSync("code.py", code);
-          testcase?.testcase.forEach((test) => {
+          testcase?.testcase.forEach((test, i) => {
+            clientResult[i] = null;
             const input = test.input;
             const command = `docker run --rm -v %cd%:/dist python:3 python /dist/code.py ${input}`;
 
             exec(command, (error, stdout, stderr) => {
               if (error) {
                 console.error(`Error: ${error.message}`);
+                socket.emit("error", error.message);
                 return;
               }
               if (stderr) {
                 console.error(`Error: ${stderr}`);
+                socket.emit("error", stderr);
                 return;
               }
 
               const result = JSON.parse(stdout);
 
               if (result === test.output) {
-                console.log("정답!");
+                clientResult[i] = true;
+                socket.emit("test", clientResult);
               } else {
-                console.log("오답!");
+                clientResult[i] = false;
+                socket.emit("test", clientResult);
               }
             });
           });
-          // fs.unlinkSync("code.py");
           break;
         default:
           return;
       }
-
-      if (result.length > 0) {
-        socket.emit("test", {
-          testcase: result,
-          result: result.some((v) => v === "false")
-            ? "테스트에 통과하지 못 했습니다."
-            : "테스트에 모두 성공했습니다.",
-        });
-      }
+      // fs.unlinkSync("code.py");
+      // fs.unlinkSync("code.js");
+      // fs.unlinkSync("input.txt");
     });
   });
 };
